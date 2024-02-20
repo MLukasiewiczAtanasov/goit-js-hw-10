df25d90b2afb981eec0c40cb819563c1f22f9a83
Magdalena <m.lukasiewicz.atanasov@gmail.com>
2024-02-20 19:30:56 +0100

    homework 10

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+// funkcja fetchCountries.js
+// Upewnij się, że API jest zdefiniowane
+const URL = 'https://restcountries.com/v3.1/name/';
+const FIELDS = 'fields=name,capital,population,flags,languages';
+
+export async function fetchCountries(name) {
+  const response = await fetch(`${URL}${name}?${FIELDS}`);
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error('Country not found');
+  }
+}
